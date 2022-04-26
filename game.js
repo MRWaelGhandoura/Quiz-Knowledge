adc0436def80d127275a41c0404606823e41d624
MRWaelGhandoura <waelghandouranew@gmail.com>
2022-04-26 21:21:14 +0000

    adding using js6 to js pages

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');

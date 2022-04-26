3815f5c1db20d2447a807550c1d43fd0b3e3dbad
MRWaelGhandoura <waelghandouranew@gmail.com>
2022-04-26 08:48:17 +0000

    creating the question array

--- game.js
+++ game.js
@@ -10,3 +10,55 @@ let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
+
+/* Creating the Question array*/
+
+let questions = [
+    {
+        question : 'What is the atomic sign for water on the periodic table ?',
+        choice1: 'O2',
+        choice2: 'CO2',
+        choice3: 'H2O',
+        choice4: 'He',
+        answer: 3,
+        
+    },
+    {
+        question : 'What is the Currency of Sweden ?',
+        choice1: 'Euro',
+        choice2: 'Pound Sterling',
+        choice3: 'Yen',
+        choice4: 'Krona',
+        answer: 4,
+        
+    },
+    {
+        question : 'What is the largest animal in the world ?',
+        choice1: 'Giraffe',
+        choice2: 'Whale',
+        choice3: 'Anaconda',
+        choice4: 'Elephant',
+        answer: 2,
+        
+    },
+    {
+        question : 'What is the longest river in the world ?',
+        choice1: 'Nile',
+        choice2: 'Mississpi',
+        choice3: 'Amazon',
+        choice4: 'Yellow River',
+        answer: 3,
+        
+    },
+    {
+        question : 'Who painted Mona Lisa ?',
+        choice1: 'Leonardo de Vinci',
+        choice2: 'Michel Angelo',
+        choice3: 'Salvador Dali',
+        choice4: 'Vincent Van Gogh',
+        answer: 3,
+        
+    }
+
+
+]

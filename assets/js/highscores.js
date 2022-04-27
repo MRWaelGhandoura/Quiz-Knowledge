936cf0efdccd1509c4f0202c6165921c18c7bb30
MRWaelGhandoura <waelghandouranew@gmail.com>
2022-04-27 22:49:00 +0000

    adding semi colomn to the javascript pages

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,7 @@
-/*jshint esversion: 6 */
-const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem("highScores")) || []
+const highScoresList = document.querySelector('#highScoresList');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML =
 highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join("")
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+}).join("");
